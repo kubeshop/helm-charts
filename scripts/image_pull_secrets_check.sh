ad712e62d16ee59537345987162a890783c1247a
Kubeshop <174873053+ed382@users.noreply.github.com>
2024-10-29 18:52:13 +0100

    feat: use forked/vendored nats chart

--- scripts/image_pull_secrets_check.sh
+++ scripts/image_pull_secrets_check.sh
@@ -6,7 +6,7 @@ helm dependency build ../charts/testkube
 # Count the number of container segments
 COUNT=$(helm template test ../charts/testkube --skip-crds --set mongodb.enabled=false --set testkube-api.minio.enabled=false --set testkube-dashboard.enabled=false --set global.testWorkflows.createOfficialTemplates=false | grep "containers:" | wc -l)
 
-echo "Discovered $COUNT images"
+echo "Discovered $COUNT container segments"
 
 COUNT_IPS1=$(helm template test ../charts/testkube --skip-crds --set mongodb.enabled=false --set testkube-api.minio.enabled=false --set testkube-dashboard.enabled=false --set global.testWorkflows.createOfficialTemplates=false --set global.imagePullSecrets="{ips1-dc41e854-73a2-4560-a549-16b01c69886b,ips2-b0cfb89a-dcad-4001-9466-c2f5ac4a1f18}" | grep ips1-dc41e854-73a2-4560-a549-16b01c69886b | wc -l)
 echo "Found $COUNT_IPS1 instances of the first pull secret"
